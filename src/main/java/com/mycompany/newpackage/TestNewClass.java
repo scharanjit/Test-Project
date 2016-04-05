96a4dae9f2de2381c66a269f06b02a7f24296a32
scharanjit <charanjit.singh@imaginea.com>
2016-04-05 14:40:19 +0530

    changes done

--- src/main/java/com/mycompany/newpackage/TestNewClass.java
+++ src/main/java/com/mycompany/newpackage/TestNewClass.java
@@ -5,21 +5,38 @@
  */
 package com.mycompany.newpackage;
 
-import java.awt.Graphics;
-
 /**
  *
  * @author charanjiths
  */
 public class TestNewClass {
+
     public static void main(String[] args) {
+        
+        
         System.out.println("Draw paint");
+        try {
+            int i = 5 / 0;
+        } catch (Exception e) {
+            System.out.println("Caught");
+        } finally {
+            paint();
+        }
+        
+        
+        
     }
 
-    private static void paint(Graphics g) {
-       
-        char []ch={'p','r','a','m','a','t','i'};
-        g.drawChars(ch, 0, 5, 25, 25);
+    private static void paint() {
+
+        char[] ch = {'p', 'r', 'a', 'm', 'a', 't', 'i'};
+        for (char c : ch) {
+            System.out.print(c);
+        }
+        System.out.println("");
+        for(char c:ch){
+            System.out.print(c+' ');
+        }
     }
-    
+
 }

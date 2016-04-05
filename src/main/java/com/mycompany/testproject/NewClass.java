18da6938e577e3acd4c1d60c5061bbded8d6826d
scharanjit <charanjit.singh@imaginea.com>
2016-04-05 13:29:35 +0530

    changes Again

--- src/main/java/com/mycompany/testproject/NewClass.java
+++ src/main/java/com/mycompany/testproject/NewClass.java
@@ -11,7 +11,7 @@ package com.mycompany.testproject;
  */
 public class NewClass {
     public static void main(String[] args){
-        System.out.println("Hello GitHub");
+        System.out.println("....Hello GitHub....");
         
     }
     

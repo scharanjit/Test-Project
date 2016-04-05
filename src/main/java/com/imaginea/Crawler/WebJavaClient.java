7b45fb6ed9fdcca4a43ca188df0b33f3bbf374f7
scharanjit <charanjit.singh@imaginea.com>
2016-04-05 17:07:02 +0530

    Added

---
+++ src/main/java/com/imaginea/Crawler/WebJavaClient.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imaginea.Crawler;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author charanjiths
+ */
+public class WebJavaClient {
+
+    void find(String currentUrl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean matchWord(String searchWord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Collection<? extends String> nextPath() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

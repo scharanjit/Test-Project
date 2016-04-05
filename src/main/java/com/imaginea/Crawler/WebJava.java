ab3cf1877b90c93c494f799869d86abe41182e19
scharanjit <charanjit.singh@imaginea.com>
2016-04-05 16:16:20 +0530

    AddedCrawlery

---
+++ src/main/java/com/imaginea/Crawler/WebJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imaginea.Crawler;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author charanjiths
+ */
+public class WebJava {
+     private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
+
+    public void download(String string, String string0) {
+    }
+    
+}
---
+++ src/main/java/com/imaginea/Crawler/WebJavaTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.imaginea.Crawler;
+
+/**
+ *
+ * @author charanjiths
+ */
+public class WebJavaTest {
+    public static void main(String[] args) {
+        WebJava w= new WebJava();
+        w.download("http://mail-archives.apache.org/mod_mbox/maven-users/","2014");
+    }
+    
+}
